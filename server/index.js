e682125abe776b4661306c0f7b382a3f3e61fe07
divcraft <t.ogonowski@wp.pl>
2020-10-23 21:28:09 +0200

    change static folder

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ module.exports = function server() {
 
    // Serve static assets if in production
    if (process.env.NODE_ENV === 'production') {
-      app.use(express.static('../client/build'));
+      app.use(express.static('client/build'));
       app.get('/ ', (req, res) => {
          res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
       });
